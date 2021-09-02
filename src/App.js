c430380bf33276f8852568a7298a8619eaf8e5cf
RAHUL KESHRI <rkeshri2021@gmail.com>
2021-09-02 15:28:49 +0530

    Footer Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import {Nav} from './components/Nav';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
           <Header/> 
           <Nav/> 
           <Main/>
+          <Footer/>
         </div>
     )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+
+import './Footer.css';
+
+function Footer() {
+    return(
+        <footer className="global-footer">
+            <p> &copy;2021-2011 All rights reserved </p>
+        </footer>
+    )
+}
+
+export default Footer;
