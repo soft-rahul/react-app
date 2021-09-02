3830314737b441f86e8c0d03cd45e63b1bfb213a
RAHUL KESHRI <rkeshri2021@gmail.com>
2021-09-02 14:24:36 +0530

    Header component is ready

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="global-header">
+            <h1> My-<span>website</span> </h1>
+        </header>
+    )
+}
+
+export default Header;
