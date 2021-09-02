3fc0e41d71c05bcd173d3585c6fd27925d3e5bce
RAHUL KESHRI <rkeshri2021@gmail.com>
2021-09-02 15:22:39 +0530

    Main is ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import {Nav} from './components/Nav';
+import Main from './components/Main';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
         <div>
           <Header/> 
           <Nav/> 
+          <Main/>
         </div>
     )
 }
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+
+import './Main.css';
+
+function Main() {
+    return <main className="global-main">
+        <h2> My content</h2>
+        <div className="card">
+        <h3>More content coming soon..</h3>
+        <p>we are working hard to deliver you best content of the world.. so please wait we are coming back with great content as soon as possible..</p>
+        </div>
+    </main>
+}
+
+export default Main;
